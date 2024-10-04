029023eb4b4dc0576e4fa1aff6112bbc716a5f8c
Jeffrey Balbalosa <31784615+jbalbalosa@users.noreply.github.com>
2024-10-04 09:54:14 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Using Array Exercises!");
+
+        int[] intArray = getRandomArray();
+
+        System.out.println("Random Array: " + Arrays.toString(intArray));
+        Arrays.sort(intArray);
+        System.out.println("Sort Ascending: " + Arrays.toString(intArray));
+
+        //Convert Array to the premitive type
+        Integer[] reverseArray = Arrays.stream(intArray).boxed().toArray(Integer[]:: new);
+
+        Arrays.sort(reverseArray, Collections.reverseOrder());
+        System.out.println("Sort Descending: " + Arrays.toString(reverseArray));
     }
+
+    private static int[] getRandomArray(){
+        Random random = new Random();
+        int[] newInt = new int[10];
+
+        for (int i = 0; i < 10; i++) {
+            newInt[i] = random.nextInt(100);
+        }
+        return  newInt;
+    }
+
 }
